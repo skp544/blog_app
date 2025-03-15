fdf7ff3532f82d48680c146fa8c9dbdfb7ecf825
skp544 <shubhamkumarprajapati544@gmail.com>
2025-03-15 20:46:09 +0530

    add routing and new pages for Home, About, SignIn, SignUp, Dashboard, and Projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Projects from "./pages/Projects.jsx";
+
 function App() {
   return (
-    <div className={"text-3xl font-bold"}>
-      <h1>App</h1>
-    </div>
+    <Routes>
+      <Route path={"/"} element={<Home />} />
+      <Route path={"/about"} element={<About />} />
+      <Route path={"/sign-in"} element={<SignIn />} />
+      <Route path={"/sign-up"} element={<SignUp />} />
+      <Route path={"/dashboard"} element={<Dashboard />} />
+      <Route path={"/projects"} element={<Projects />} />
+    </Routes>
   );
 }
 
